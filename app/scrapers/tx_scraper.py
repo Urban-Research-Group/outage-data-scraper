cac604c600a25ca52c38321b0c14fe21af110d8a
Raks <spotluri31@gatech.edu>
2023-03-13 10:09:17 -0400

    Texas Scraper Updates

--- app/scrapers/tx_scraper.py
+++ app/scrapers/tx_scraper.py
@@ -139,6 +139,61 @@ class BaseScraper:
         return driver
 
 
+class Scraper5(BaseScraper):
+    def __init__(self, url, emc):
+        super().__init__(url, emc)
+        self.driver = self.init_webdriver()
+
+    def parse(self):
+        data = self.fetch()
+
+        for key, val in data.items():
+            if val:
+                df = pd.DataFrame(val['areas'])
+                df[['cust_a', 'percent_cust_a']] = df[['cust_a', 'percent_cust_a']].applymap(lambda x : x['val'])
+                df['timestamp'] = timenow()
+                df['EMC'] = self.emc
+                df.drop(columns=['gotoMap'], inplace=True)
+                data.update({key: df})
+            else:
+                print(f"no outage of {self.emc} update found at",
+                      datetime.strftime(datetime.now(), "%m-%d-%Y %H:%M:%S"))
+        return data
+
+
+    def fetch(self):
+        print(f"fetching {self.emc} outages from {self.url}")
+        # get javascript rendered source page
+        self.driver.get(self.url)
+        # Sleeps for 5 seconds
+        time.sleep(3)
+        page_source = self.driver.page_source
+
+        # parse reports link
+        soup = BeautifulSoup(page_source, "html.parser")
+        containers = soup.find_all(class_="row report-link hyperlink-primary")
+        links = {}
+        for c in containers:
+            links.update({c.get("data-metrics-label"): c.get("href")})
+
+        # get json reports
+        raw_data = {}
+        for k, v in links.items():
+            self.driver.get(self.url+v[1:])
+            time.sleep(3)
+            requests = self.driver.requests
+            for r in requests:
+                if v in r.url:
+                    print(r)
+                    response = sw_decode(r.response.body,
+                                         r.response.headers.get('Content-Encoding', 'identity'))
+                    data = response.decode("utf8", 'ignore')
+                    if any([x in data for x in ['zip', 'Zip', 'City']]):
+                        raw_data['per_zipcode'] = json.loads(data)['file_data']
+                    elif 'county' in data:
+                        raw_data['per_county'] = json.loads(data)['file_data']
+        return raw_data
+
 class Scraper8(BaseScraper):
     def __init__(self, url, emc):
         super().__init__(url, emc)
@@ -175,14 +230,66 @@ class Scraper8(BaseScraper):
 
         return raw_data
 
+class Scraper11(BaseScraper):
+    def __init__(self, url, emc):
+        super().__init__(url, emc)
+        self.driver = self.init_webdriver()
+
+    def parse(self):
+        self.fetch()
+        soup = BeautifulSoup(self.driver.page_source, "html.parser")
+        tables = soup.find_all("table")
+
+        # Find the table rows using their tag name
+        rows = tables[1].find_all('tr')
+
+        # Extract the table header row
+        header_row = rows[0]
+
+        # Extract the table data rows
+        data_rows = rows[1:]
+
+        # Extract the table header cells
+        header_cells = header_row.find_all('th')
+        header = [cell.get_text().strip() for cell in header_cells]
+
+        # Extract the table data cells
+        data = []
+        for row in data_rows:
+            cells = row.find_all('td')
+            data.append([cell.get_text().strip() for cell in cells])
+
+        # Print the table data as a list of dictionaries
+        table_data = [dict(zip(header, row)) for row in data]
+        df = pd.DataFrame(table_data)[:-1][['County', '# Out', '# Served', '% Out']]
+        df['timestamp'] = timenow()
+        df['EMC'] = self.emc
+        data = {'per_county': df}
+        return data
+
+    def fetch(self):
+        print(f"fetching {self.emc} outages from {self.url}")
+        # Send a request to the website and let it load
+        self.driver.get(self.url)
+        time.sleep(10)
+        # WebDriverWait(self.driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "ptifrmtgtframe")))
+        # WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[5]/div[2]/div/div/div[3]/div/div[2]/div/div/div/div[1]/table")))
+        print(self.driver.page_source)
+
+
+
 
 
 
 
 class TXScraper:
     def __new__(cls, layout_id, url, emc):
-        if layout_id == 8:
+        if layout_id == 5:
+            obj = super().__new__(Scraper5)        
+        elif layout_id == 8:
             obj = super().__new__(Scraper8)
+        elif layout_id == 11:
+            obj = super().__new__(Scraper11)
         else:
             raise "Invalid layout ID: Enter layout ID range from 1 to 11"
 
@@ -192,7 +299,7 @@ class TXScraper:
 
 if __name__ == "__main__":
 
-    sc = TXScraper(layout_id=8,
-                url='http://outage.bluebonnetelectric.coop:82/',
-                emc='Blue Bonnet')
+    sc = TXScraper(layout_id=5,
+                url='https://stormcenter.oncor.com/default.html',
+                emc='Storm Center')
     print(sc.parse())
