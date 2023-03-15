6cf73fd2ae4a886691317637d01a55f87a87539c
rickonz <49249770+rickonz@users.noreply.github.com>
2023-03-15 03:13:59 -0400

    drop duplication

--- app/scrapers/util.py
+++ app/scrapers/util.py
@@ -1,6 +1,7 @@
 import os
 import boto3
 import io
+import pandas as pd
 
 from datetime import datetime
 from urllib.error import HTTPError, URLError
@@ -11,29 +12,50 @@ def is_aws_env():
     return os.environ.get('AWS_LAMBDA_FUNCTION_NAME') or os.environ.get('AWS_EXECUTION_ENV')
 
 
+def is_duplicate(df, file):
+    # TODO: per_outage
+    # read .csv -> append non-duplicate entries
+    pass
+
+
 def save(df, bucket_name=None, file_path=None):
     if is_aws_env():
-        s3_client = boto3.client("s3")
-        with io.StringIO() as csv_buffer:
-            df.to_csv(csv_buffer, index=False)
-
-            response = s3_client.put_object(
-                Bucket=bucket_name, Key=file_path, Body=csv_buffer.getvalue()
-            )
-
+        # Check if the file exists in the bucket
+        s3_client = boto3.client('s3')
+        try:
+            s3_client.head_object(Bucket=bucket_name, Key=file_path)
+        except:
+            # initial save
+            s3_resource = boto3.resource('s3')
+            s3_object = s3_resource.Object(bucket_name, file_path)
+            response = s3_object.put(Body=df.to_csv(index=False), ContentType='text/csv')
             status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+            return f"new outages table initialized at {file_path}. Status - {status}"
 
-            if status == 200:
-                print(f"Successful S3 put_object response. Status - {status}")
-            else:
-                print(f"Unsuccessful S3 put_object response. Status - {status}")
+        else:
+            # combine and drop duplicate
+            s3_object = s3_client.get_object(Bucket=bucket_name, Key=file_path)
+            df_og = pd.read_csv(io.BytesIO(s3_object['Body'].read()))
+            df = df_og.append(df, ignore_index=True)
+            df.drop_duplicates(inplace=True)
+
+            # update to s3
+            with io.StringIO() as csv_buffer:
+                df.to_csv(csv_buffer, index=False)
+
+                response = s3_client.put_object(
+                    Bucket=bucket_name, Key=file_path, Body=csv_buffer.getvalue()
+                )
+                status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+
+            return f"{bucket_name} outages updated to {file_path}. Status - {status}"
+
     else:
+        # TODO: drop duplicate save
         local_path = f"{os.getcwd()}/../{bucket_name}/{file_path}"
         df.to_csv(local_path, index=False)
         print(f"outages data saved to {file_path}")
 
-def check_duplicate():
-    pass
 
 def make_request(url, headers=None):
     # TODO: refactor all 'urlopen'
@@ -52,6 +74,7 @@ def make_request(url, headers=None):
         print(error.reason)
     except TimeoutError:
         print("Request timed out")
+
 
 def timenow():
     return datetime.strftime(datetime.now(), "%m-%d-%Y %H:%M:%S")
