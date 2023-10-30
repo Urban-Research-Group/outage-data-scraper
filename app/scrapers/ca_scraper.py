55991c04c4069782d002baf5d3c96e41580bdb99
Gting6 <b07901090@ntu.edu.tw>
2023-10-29 21:15:57 -0400

    Fix ca-outage-cpa

--- app/scrapers/ca_scraper.py
+++ app/scrapers/ca_scraper.py
@@ -2,7 +2,7 @@ import pandas as pd
 import requests
 import time
 
-from .ga_scraper import BaseScraper, Scraper9 as GA_Scraper9
+from .ga_scraper import BaseScraper, Scraper9 as GA_Scraper9, Scraper1 as GA_Scraper1
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -19,15 +19,28 @@ class ScraperINV(BaseScraper):
         data = self.fetch()
         # parsing
         for key, val in data.items():
-            df = pd.DataFrame(val['features'])
-            df = pd.concat([df.drop(["attributes"], axis=1), df["attributes"].apply(lambda x: pd.Series(x))], axis=1)
-            df = pd.concat([df.drop(["geometry"], axis=1), df["geometry"].apply(lambda x: pd.Series(x))], axis=1)
+            df = pd.DataFrame(val["features"])
+            df = pd.concat(
+                [
+                    df.drop(["attributes"], axis=1),
+                    df["attributes"].apply(lambda x: pd.Series(x)),
+                ],
+                axis=1,
+            )
+            df = pd.concat(
+                [
+                    df.drop(["geometry"], axis=1),
+                    df["geometry"].apply(lambda x: pd.Series(x)),
+                ],
+                axis=1,
+            )
 
-            df['timestamp'] = timenow()
+            df["timestamp"] = timenow()
             # disabled b/c takes too long
             # df['zip'] = df.apply(lambda x: self.extract_zipcode(x.y, x.x), axis=1)
-            df[['StartDate', 'EstimatedRestoreDate']] = df[['StartDate', 'EstimatedRestoreDate']].apply(pd.to_datetime,
-                                                                                                        unit='ms')
+            df[["StartDate", "EstimatedRestoreDate"]] = df[
+                ["StartDate", "EstimatedRestoreDate"]
+            ].apply(pd.to_datetime, unit="ms")
             data.update({key: df})
 
         return data
@@ -35,12 +48,7 @@ class ScraperINV(BaseScraper):
     def fetch(self):
         raw_data = {}
         # Query parameters
-        params = {
-            "where": "1=1",
-            "outFields": "*",
-            "outSR": "4326",
-            "f": "json"
-        }
+        params = {"where": "1=1", "outFields": "*", "outSR": "4326", "f": "json"}
 
         # Send a GET request to the API endpoint with the query parameters
         response = requests.get(self.url, params=params)
@@ -48,7 +56,7 @@ class ScraperINV(BaseScraper):
         # Check if the request was successful
         if response.status_code == 200:
             # Extract the JSON response content
-            raw_data.update({'per_outage': response.json()})
+            raw_data.update({"per_outage": response.json()})
 
             return raw_data
         else:
@@ -69,11 +77,11 @@ class ScraperCC(BaseScraper):
 
 class CAScraper:
     def __new__(cls, layout_id, url, emc):
-        if layout_id == 'investor':
+        if layout_id == "investor":
             obj = super().__new__(ScraperINV)
-        elif layout_id == 'paloalto':
-            obj = super().__new__(GA_Scraper9)
-        elif layout_id == 'colton':
+        elif layout_id == "paloalto":
+            obj = super().__new__(GA_Scraper1)
+        elif layout_id == "colton":
             obj = super().__new__(ScraperCC)
         else:
             raise "Invalid layout ID: Enter layout ID in ['investor', 'cpa', 'cc']"
