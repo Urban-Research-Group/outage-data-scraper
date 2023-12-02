06b3ced16da83900a510c53a0a34dbb4174fb933
Gting6 <b07901090@ntu.edu.tw>
2023-12-02 18:05:48 -0500

    Add il_5, update il_3's json

--- app/scrapers/il_scraper.py
+++ app/scrapers/il_scraper.py
@@ -19,7 +19,12 @@ from seleniumwire import webdriver
 
 from .util import is_aws_env, make_request, timenow
 
-from .ga_scraper import BaseScraper, Scraper1 as GA_Scraper1, Scraper9 as GA_Scraper9
+from .ga_scraper import (
+    BaseScraper,
+    Scraper1 as GA_Scraper1,
+    Scraper9 as GA_Scraper9,
+    Scraper3 as GA_Scraper3,
+)
 
 
 # TODO: update for security
@@ -235,6 +240,8 @@ class ILScraper:
             obj = super().__new__(GA_Scraper9)
         elif layout_id == 4:
             obj = super().__new__(GA_Scraper1)
+        elif layout_id == 5:
+            obj = super().__new__(GA_Scraper3)
         else:
             raise "Invalid layout ID: Enter layout ID range from 1 to 2"
         obj.__init__(url, emc)
