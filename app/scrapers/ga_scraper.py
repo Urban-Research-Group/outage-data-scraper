65cc6b75ee788e6d7db26a8cfb2e2c54698f5732
Gting6 <b07901090@ntu.edu.tw>
2024-01-06 23:57:56 +0800

    Fix bug in GA-5

--- app/scrapers/ga_scraper.py
+++ app/scrapers/ga_scraper.py
@@ -355,8 +355,8 @@ class Scraper5(BaseScraper):
             if key == "per_outage" and val:
                 df = pd.DataFrame(val)
                 df["timestamp"] = timenow()
-                df[["startTime", "lastUpdatedTime", "etrTime"]] = df[
-                    ["startTime", "lastUpdatedTime", "etrTime"]
+                df[["startTime", "lastUpdatedTime"]] = df[
+                    ["startTime", "lastUpdatedTime"]
                 ].apply(pd.to_datetime, unit="ms")
                 df["EMC"] = self.emc
                 df["zip_code"] = df.apply(
