b7942fad7d6bdce613912b8c5d5a80d3306dab35
Suryaa <suryaacoolness@gmail.com>
2024-03-31 15:14:51 -0400

    nc dominion

--- app/scrapers/nc_scraper.py
+++ app/scrapers/nc_scraper.py
@@ -121,6 +121,102 @@ class Scraper6(BaseScraper):
 
         return table_data
 
+class Scraper7(BaseScraper):
+    def __init__(self, url, emc):
+        super().__init__(url, emc)
+        self.driver = self.init_webdriver()
+
+    def parse(self):
+        data = self.fetch()
+        for key, val in data.items():
+            print(key)
+            if val:
+                df = pd.DataFrame(val)
+                df["timestamp"] = timenow()
+                df["EMC"] = self.emc
+                data.update({key: df})
+            else:
+                print(
+                    f"no '{key}' outage of {self.emc} update found at",
+                    datetime.strftime(datetime.now(), "%m-%d-%Y %H:%M:%S"),
+                )
+
+        self.driver.close()
+        self.driver.quit()
+
+        return data
+    
+    def fetch(self):
+        print(f"Fetching {self.emc} outages from {self.url}")
+        # get javascript rendered source page
+        self.driver.get(self.url)
+        # Sleeps for 5 seconds
+        time.sleep(5)
+
+        try:
+            menu_button = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.ID, 'btn-menu'))
+            )
+    
+            # Click on the menu button
+            menu_button.click()
+
+            print("clicked menu!")
+
+            summary_icon = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.ID, 'summary-icon'))
+            )
+            
+            # Click on the summary icon
+            summary_icon.click()
+
+
+
+            # Click on the arrow to expand the menu
+            
+            # Wait for the "Area / County" link to be clickable
+            area_county_link = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.ID, 'view-summary-county-muni'))
+            )
+            
+            # Click on the "Area / County" link
+            area_county_link.click()
+
+            print("clicked summary!")
+
+            nc_element = WebDriverWait(self.driver, 10).until(
+                EC.element_to_be_clickable((By.XPATH, '//span[text()="North Carolina"]'))
+            )
+            
+            # Click on the expand/collapse arrow for North Carolina
+            arrow_element = nc_element.find_element(By.XPATH, './preceding-sibling::span[@class="treegrid-expander treegrid-expander-collapsed"]')
+            arrow_element.click()
+
+            print("clicked nc!")
+
+            rows = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.treegrid-parent-1.level2'))
+            )
+            
+            # Create a dictionary to store the data
+            data = {}
+
+            # Loop through each row and extract the data
+            for row in rows:
+                area_name = row.find_element(By.CSS_SELECTOR, '.area_nameCol span:nth-child(3)').text        
+                cust_a = row.find_element(By.CSS_SELECTOR, '.cust_aCol span').text
+                cust_s = row.find_element(By.CSS_SELECTOR, '.cust_sCol span').text
+                if (cust_a != '0'):
+                    data[area_name] = {'cust_a': cust_a, 'cust_s': cust_s}
+            
+        except Exception as e:
+            print(f"Error: {e}")
+            self.driver.close()
+            self.driver.quit()
+
+        return data
+
+
 class NCScraper:
     def __new__(cls, layout_id, url, emc):
         if layout_id == 1:
