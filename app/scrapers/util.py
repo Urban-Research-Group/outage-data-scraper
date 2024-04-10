2bdff5b582f09b917ff2f1b80415403aff3358b5
Gting6 <b07901090@ntu.edu.tw>
2024-04-10 00:25:39 -0400

    Fix stuff

--- app/scrapers/util.py
+++ app/scrapers/util.py
@@ -54,7 +54,7 @@ def save(df, bucket_name=None, file_path=None):
 
     else:
         # TODO check path
-        local_path = f"{os.getcwd()}/{file_path}"
+        local_path = f"{os.getcwd()}/../{bucket_name}/local/{file_path}"
         header = False if os.path.exists(local_path) else True
         df.to_csv(local_path, mode="a", header=header, index=False)
         print(f"outages data saved to {file_path}")
