77c7bb0b16e32a6fa439b57778fc56716250661f
Joseph Seo <134018750+seojoseph4@users.noreply.github.com>
2024-04-19 13:33:18 -0400

    Pipeline implementation for tx10 and tx6

--- app/pipeline/pipeline.py
+++ app/pipeline/pipeline.py
@@ -240,11 +240,165 @@ class TX4(BasePipeline):
         pass
     
 class TX6(BasePipeline):
+    def transform(self, **kwargs):
+        identifiers = kwargs.get('identifiers')
+        method = kwargs.get('method')
+        geo_level = kwargs.get('geo_level')
+        time_interval = kwargs.get('time_interval')
+        df = kwargs.get('dataframe', self._data.copy())
+        
+        df = df.rename(columns={
+            'id':'outage_id',
+            'CUSTOMERSOUT':'customer_affected',
+            'EMC':'utility_provider',
+            'x':'longitude',
+            'y':'latitude'
+        })
+        
+        # Convert timestamps
+        eastern = tz.gettz('US/Eastern')
+        df['BEGINTIME'] = pd.to_datetime(df['BEGINTIME'], utc=True, errors='coerce').dt.tz_convert(eastern)
+        df['BEGINTIME'] = pd.to_datetime(df['BEGINTIME'], utc=True).dt.tz_convert(eastern)
+        df['timestamp'] = pd.to_datetime(self._data['timestamp'], utc=True, errors='coerce').dt.tz_convert(eastern)
+        df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True).dt.tz_convert(eastern)
+        df['ESTIMATEDTIMERESTORATION'] = pd.to_datetime(self._data['ESTIMATEDTIMERESTORATION'], utc=True, errors='coerce').dt.tz_convert(eastern)
+        df['ESTIMATEDTIMERESTORATION'] = pd.to_datetime(df['ESTIMATEDTIMERESTORATION'], utc=True).dt.tz_convert(eastern)
+
+        df['start_time'] = df['BEGINTIME']
+        # extract lat and long
+        # df['outagePoint'] = df['outagePoint'].apply(lambda x: json.loads(x.replace("'", '"')))
+        # df[['latitude', 'longitude']] = df['outagePoint'].apply(lambda x: pd.Series([x['lat'], x['lng']]))
+        # TODO: add zipcode NaN checking
+        
+        if identifiers:
+            if method == 'timegap_seperation': 
+                # subgroup by timegap rule, must derive subgroup step by step
+                df['gap'] = (df.groupby(identifiers)['timestamp'].diff().dt.total_seconds() / 60).round(0).fillna(0)
+                df['is_gap'] = (df.groupby(identifiers)['timestamp'].diff() > pd.Timedelta(minutes=20))
+                df['subgroup'] = df.groupby(identifiers)['is_gap'].cumsum()
+                identifiers.append('subgroup')
+                
+            df['id'] = df[identifiers].apply(tuple, axis=1)
+        
+        # for zipcode level transformation 
+        if geo_level and time_interval:
+            # TODO: complet geo_level and time_interval support
+            keys = ['zipcode', 'year', 'month', 'day', 'hour'] 
+            df['customer_served'] = 0
+            df['percent_customer_affected'] = 0
+            
+            # Aggregate count of unique outage_id
+            count_df = df.groupby(keys)['outage_id'].nunique().reset_index()
+            count_df.rename(columns={'outage_id': 'outage_count'}, inplace=True)
+
+            # Merge aggregated DataFrame with the original DataFrame
+            df = pd.merge(df, count_df, on=keys, how='left')
+        return df
+    
+    # TODO: refactor such that overriden method only include extra variables
+    def _agg_vars(self, group):
+        first_timestamp = group['timestamp'].iloc[0]
+        last_timestamp = group['timestamp'].iloc[-1]
+        duration_diff = (last_timestamp - first_timestamp).total_seconds() / 60
+        duration_15 = 15 * len(group)
+        group['duration_weight'] = (group['timestamp'].diff().dt.total_seconds() / 60).round(0).fillna(15)
+        cust_affected_x_duration = (group['customer_affected'] * group['duration_weight']).sum()
+        cust_a_mean = cust_affected_x_duration / group['duration_weight'].sum()
+        
+        return pd.Series({
+            'latitude': group['latitude'].unique(),
+            'longitude': group['longitude'].unique(),
+            # 'zipcode': group['zipcode'].unique(),
+            'start_time': group['start_time'].unique(),
+            'first_timestamp': first_timestamp,
+            'last_timestamp': last_timestamp,
+            'duration_diff': duration_diff,
+            'duration_15': duration_15,
+            'customer_affected_mean': cust_a_mean,
+            'cust_affected_x_duration': cust_affected_x_duration
+        })
     def standardize(self, outage_data):
         # Specific transformation for TX6
         pass
 
 class TX10(BasePipeline):
+    def transform(self, **kwargs):
+        identifiers = kwargs.get('identifiers')
+        method = kwargs.get('method')
+        geo_level = kwargs.get('geo_level')
+        time_interval = kwargs.get('time_interval')
+        df = kwargs.get('dataframe', self._data.copy())
+        
+        df = df.rename(columns={
+            'id':'outage_id',
+            'custAffected':'customer_affected',
+            'zip':'zipcode',
+            'EMC':'utility_provider',
+            'lon':'longitude',
+            'lat':'latitude'
+        })
+        
+        # Convert timestamps
+        eastern = tz.gettz('US/Eastern')
+        df['date'] = pd.to_datetime(df['date'].str[:-4], format='%B %d, %Y %I:%M %p', errors='coerce')
+        df['date'] = df['date'].dt.tz_localize(None).dt.tz_localize('UTC').dt.tz_convert(eastern)
+        df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True, errors='coerce').dt.tz_convert(eastern)
+        df['timestamp'] = pd.to_datetime(df['timestamp'], utc=True).dt.tz_convert(eastern)
+        df['start_time'] = df['date']
+        
+        # extract lat and long
+        # df['outagePoint'] = df['outagePoint'].apply(lambda x: json.loads(x.replace("'", '"')))
+        # df[['latitude', 'longitude']] = df['outagePoint'].apply(lambda x: pd.Series([x['lat'], x['lng']]))
+        # TODO: add zipcode NaN checking
+        
+        if identifiers:
+            if method == 'timegap_seperation': 
+                # subgroup by timegap rule, must derive subgroup step by step
+                df['gap'] = (df.groupby(identifiers)['timestamp'].diff().dt.total_seconds() / 60).round(0).fillna(0)
+                df['is_gap'] = (df.groupby(identifiers)['timestamp'].diff() > pd.Timedelta(minutes=20))
+                df['subgroup'] = df.groupby(identifiers)['is_gap'].cumsum()
+                identifiers.append('subgroup')
+                
+            df['id'] = df[identifiers].apply(tuple, axis=1)
+        
+        # for zipcode level transformation 
+        if geo_level and time_interval:
+            # TODO: complet geo_level and time_interval support
+            keys = ['zipcode', 'year', 'month', 'day', 'hour'] 
+            df['customer_served'] = 0
+            df['percent_customer_affected'] = 0
+            
+            # Aggregate count of unique outage_id
+            count_df = df.groupby(keys)['outage_id'].nunique().reset_index()
+            count_df.rename(columns={'outage_id': 'outage_count'}, inplace=True)
+
+            # Merge aggregated DataFrame with the original DataFrame
+            df = pd.merge(df, count_df, on=keys, how='left')
+        
+        return df
+    
+    # TODO: refactor such that overriden method only include extra variables
+    def _agg_vars(self, group):
+        first_timestamp = group['timestamp'].iloc[0]
+        last_timestamp = group['timestamp'].iloc[-1]
+        duration_diff = (last_timestamp - first_timestamp).total_seconds() / 60
+        duration_15 = 15 * len(group)
+        group['duration_weight'] = (group['timestamp'].diff().dt.total_seconds() / 60).round(0).fillna(15)
+        cust_affected_x_duration = (group['customer_affected'] * group['duration_weight']).sum()
+        cust_a_mean = cust_affected_x_duration / group['duration_weight'].sum()
+        
+        return pd.Series({
+            'latitude': group['latitude'].unique(),
+            'longitude': group['longitude'].unique(),
+            'zipcode': group['zipcode'].unique(),
+            'start_time': group['start_time'].unique(),
+            'first_timestamp': first_timestamp,
+            'last_timestamp': last_timestamp,
+            'duration_diff': duration_diff,
+            'duration_15': duration_15,
+            'customer_affected_mean': cust_a_mean,
+            'cust_affected_x_duration': cust_affected_x_duration
+        })
     def standardize(self, outage_data):
         # Specific transformation for TX10
         pass
