e6b6e5640f596cbe8a371d9000385a4cc08c1125
Gting6 <b07901090@ntu.edu.tw>
2024-04-23 12:43:05 -0400

    Write to scraper downtime whenever failed

--- app/main.py
+++ app/main.py
@@ -5,6 +5,8 @@ import os
 from scrapers import Scraper
 from scrapers.util import save, timenow
 
+import pandas as pd
+
 
 def handler(event, context=""):
     layout_id = event["layout"]
@@ -12,7 +14,7 @@ def handler(event, context=""):
     bucket = event["bucket"]
     state = event["folder"]
     success_cnt = 0
-    failures = []
+    failures = {"state": [], "layout": [], "emc": [], "reason": [], "timestamp": []}
 
     for emc, url in EMCs.items():
         try:
@@ -20,22 +22,33 @@ def handler(event, context=""):
             data = sc.parse()
             for key, df in data.items():
                 if df.empty:
-
                     print(f"no {key} outages for {emc} as of {timenow()}")
                 else:
                     path = f"{state}/layout_{layout_id}/{key}_{emc}.csv"
                     save(df, bucket, path)
             success_cnt += 1
         except Exception as e:
-            failure_reason = f"{state} Failed to scrape {emc}: {str(e)}"
-            print(failure_reason)
-            failures.append(failure_reason)
+            failures["state"].append(state)
+            failures["layout"].append(layout_id)
+            failures["emc"].append(emc)
+            failures["reason"].append(str(e))
+            failures["timestamp"].append(timenow())
+
+            print(f"{state} Failed to scrape {emc}: {str(e)}")
+
             continue
 
-    if failures:
-        print("Failures:")
-        for failure in failures:
-            print(failure)
+    # save failures to csv
+    if len(failures["state"]) > 0:
+        df = pd.DataFrame(failures)
+        bucket = "scraperdowntime"
+        path = f"{state}/layout_{layout_id}/failures.csv"
+        save(df, bucket, path)
+
+    # if len(failures["state"]) > 0:
+    #     print("Failures:")
+    #     for failure in failures:
+    #         print(failure["emc"], failure["reason"])
 
     print(
         f"Successfully scraped {success_cnt} out of {len(EMCs)} EMC outages for {state}"
@@ -53,7 +66,7 @@ if __name__ == "__main__":
     start = time.time()
 
     # handler test here
-    event_path = os.path.join(os.getcwd(), "events/ga/layout_11.json")
+    event_path = os.path.join(os.getcwd(), "../events/tn/test.json")
     with open(event_path) as f:
         test_event = json.loads(f.read())
     handler(test_event)
