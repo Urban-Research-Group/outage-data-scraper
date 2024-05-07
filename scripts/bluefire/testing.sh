58c4e3c362491702def6dc53191dd8c1d6ff97c4
Gting6 <b07901090@ntu.edu.tw>
2024-05-07 13:35:11 -0700

    Modify output route of step2

--- scripts/bluefire/testing.sh
+++ scripts/bluefire/testing.sh
@@ -18,15 +18,15 @@ python3 step1_ganz.py -l error -f $1 -t 0.2
 
 echo "Threshold methods are done, Calculating properties"
 
-python3 step2_property.py -m threshold -f $1 -t 0.1 -a 5 > threshold.txt
-python3 step2_property.py -m threshold -f $1 -t 0.1 -a 10 >> threshold.txt
-python3 step2_property.py -m threshold -f $1 -t 0.05 -a 5 >> threshold.txt
-python3 step2_property.py -m threshold -f $1 -t 0.05 -a 10 >> threshold.txt
-python3 step2_property.py -m threshold -f $1 -t 0.01 -a 5 >> threshold.txt
-python3 step2_property.py -m threshold -f $1 -t 0.01 -a 10 >> threshold.txt
+python3 step2_property.py -m threshold -f $1 -t 0.1 -a 5 > step2/threshold.txt
+python3 step2_property.py -m threshold -f $1 -t 0.1 -a 10 >> step2/threshold.txt
+python3 step2_property.py -m threshold -f $1 -t 0.05 -a 5 >> step2/threshold.txt
+python3 step2_property.py -m threshold -f $1 -t 0.05 -a 10 >> step2/threshold.txt
+python3 step2_property.py -m threshold -f $1 -t 0.01 -a 5 >> step2/threshold.txt
+python3 step2_property.py -m threshold -f $1 -t 0.01 -a 10 >> step2/threshold.txt
 
-python3 step2_property.py -m ganz -f $1 -t 0.05  > ganz.txt
-python3 step2_property.py -m ganz -f $1 -t 0.1  >> ganz.txt
-python3 step2_property.py -m ganz -f $1 -t 0.2  >> ganz.txt
+python3 step2_property.py -m ganz -f $1 -t 0.05  > step2/ganz.txt
+python3 step2_property.py -m ganz -f $1 -t 0.1  >> step2/ganz.txt
+python3 step2_property.py -m ganz -f $1 -t 0.2  >> step2/ganz.txt
 
 echo "Finished!"
